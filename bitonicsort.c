3a6fcb222c0e39be4bdc66e74ecc163c246add57
faisal-almashouq <faisalmashouq@mawj.ai>
2025-11-22 02:54:18 +0300

    first commit, added main structure. Ensured data to be of even sized for bitonic merge sort to work.

---
+++ bitonicsort.c
@@ -0,0 +1,29 @@
+// Copyright of Faisal Almashouq 444105697
+#include <stdio.h>
+#include <stdlib.h>
+
+void load_data(int** data) {
+    printf("Loading data");
+}
+
+int** bitonic_sort(int** data) {
+    printf("Sorting data using Bitonic Merge Sort Algorithm");
+    return data;
+}
+int main() {
+    int **unsorted;
+    load_data(unsorted);
+    if ((sizeof(unsorted)/sizeof(unsorted[0])) % 2 != 0) {
+        printf("Array length must be even");
+        return -1;
+    }
+    
+
+    int** sorted = bitonic_sort(unsorted);
+    for (int i = 0; i < sizeof(sorted)/sizeof(sorted[0]); i++) {
+        printf("The new sorted data: %d ", sorted[i]);
+    }
+    
+    
+    return 0;
+}
