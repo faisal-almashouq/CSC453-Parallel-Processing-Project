470a5dfd3ec4fa4b280ff95fd33dbf4e595a20d1
faisal-almashouq <faisalmashouq@mawj.ai>
2025-11-22 06:19:50 +0300

    finally fixed .cu code. It is finally running.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ This is a project for my CSC453 Parallel Processing course. Acknowledgement: Tha
 You can read the journal for this project on this google docs link:
 https://docs.google.com/document/d/1yXXVxGknHjpkKU-PKfMZc-pyPwdt-9pch3542H3eBUU/edit?tab=t.0
 
+Note: To run .cu file, you must have the following:
+nvcc compiler (MSVC)
+cl (CUDA kit)
+
+This tiny piece of information took me 2 hours to learn and understand.
--- cuda_learn.cu
+++ cuda_learn.cu
@@ -12,32 +12,37 @@ __global__ void kernel_add(int *a, int *b, int *c) {
 }
 
 int main () {
-    int *a, *b, *c, *d_a, *d_b, *d_c;
+    int *a, *b, *c, *d_a = 0, *d_b = 0, *d_c = 0;
     int size = N * sizeof(int);
 
-    a = (int *)malloc(size);
-    b = (int *)malloc(size);
-    c = (int *)malloc(size);
+    a = (int*)malloc(size);
+    b = (int*)malloc(size);
+    c = (int*)malloc(size);
 
     for (int i = 0; i < N; i++) {
         a[i] = i;
         b[i] = i * 2;
     }
 
-
-
-    cudaMalloc((int*)d_a, size);
-    cudaMalloc((int*)d_b, size);
-    cudaMalloc((int*)d_c, size);
+    cudaMalloc((void**)&d_a, size);
+    cudaMalloc((void**)&d_b, size);
+    cudaMalloc((void**)&d_c, size);
 
     cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    
     kernel_add<<<(N + 255) / 256, 256>>>(d_a, d_b, d_c);
 
+    cudaDeviceSynchronize();
+
     cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+    cudaDeviceSynchronize();
+
     for (int i = 0; i < 10; i++) {
         printf("%d + %d = %d\n", a[i], b[i], c[i]);
     }
+    
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
